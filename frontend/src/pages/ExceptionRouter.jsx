01274b17354e5002db210422c98253140de7e4ea
josiahalora-beep <josiahalora@gmail.com>
2026-07-10 05:19:10 -0400

    PR15: route ticket issues to operator-language detail

--- frontend/src/pages/ExceptionRouter.jsx
+++ frontend/src/pages/ExceptionRouter.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import ExceptionDetail from "@/pages/ExceptionDetail";
+import TicketIssueDetail from "@/pages/TicketIssueDetail";
 import RouteIssueDetail from "@/pages/RouteIssueDetail";
+import { routeExceptionQueue } from "@/data/mockData";
 import { routeIntelligenceExceptions } from "@/data/routeIntelligenceData";
 
 export default function ExceptionRouter() {
   const { id } = useParams();
   const routeIssue = routeIntelligenceExceptions.find((item) => item.id === id);
+  const ticketIssue = routeExceptionQueue.find((item) => item.id === id);
 
   if (routeIssue) {
     return <RouteIssueDetail issue={routeIssue} />;
+  }
+
+  if (ticketIssue) {
+    return <TicketIssueDetail issue={ticketIssue} />;
   }
 
   return <ExceptionDetail />;
